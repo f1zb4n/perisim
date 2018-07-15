7472722bcfd344a516a3730da0c0bdb71701699f
Marco Werner <marco.werner@dpdhl.com>
2018-07-15 09:36:10 +0200

    add generated files from spring initializr

---
+++ src/main/java/de/clustscape/perisim/PerisimApplication.java
@@ -0,0 +1,12 @@
+package de.clustscape.perisim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerisimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerisimApplication.class, args);
+	}
+}
